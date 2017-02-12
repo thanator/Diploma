b1c256741085f976da42d036820917e0a4063ee2
thanator <www.godx@mail.ru>
2017-02-12 18:11:09 +0300

    11 less

--- test_1.py
+++ test_1.py
@@ -2,18 +2,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 import urllib
 import matplotlib.dates as mdates
+import datetime as dt
 
-def bytespdate2num(fmt,encoding='utf-8'):
+
+def bytespdate2num(fmt, encoding='utf-8'):
     strconventer = mdates.strpdate2num(fmt)
+
     def bytesconventer(b):
         s = b.decode(encoding)
         return strconventer(s)
     return bytesconventer
 
+
 def graph_data(stock):
-    
+
     fig = plt.figure()
-    ax1 = plt.subplot2grid((1,1),(0,0))
+    ax1 = plt.subplot2grid((1, 1), (0, 0))
 
     stock_price_url = 'http://chartapi.finance.yahoo.com/instrument/1.0/' + \
         stock + '/chartdata;type=quote;range=10y/csv'
@@ -30,18 +34,25 @@ def graph_data(stock):
 
     date, closep, highp, lowp, openp, volume = np.loadtxt(stock_data,
                                                           delimiter=',',
-                                                          unpack=True,
-                                                          converters={0: bytespdate2num('%Y%m%d')})
+                                                          unpack=True)
 
-    ax1.plot_date(date, closep, '-', label = 'Price')
+    dateconv = np.vectorize(dt.datetime.fromtimestamp)
+    date = dateconv(date)
+    # date, closep, highp, lowp, openp, volume = np.loadtxt(stock_data,
+    #                                                       delimiter=',',
+    #                                                       unpack=True,
+    # converters={0: bytespdate2num('%Y%m%d')})
+
+    ax1.plot_date(date, closep, '-', label='Price')
     for label in ax1.xaxis.get_ticklabels():
         label.set_rotation(45)
-        ax1.grid(True)#, color='g',linestyle='-')
+        ax1.grid(True)  # , color='g',linestyle='-')
     plt.xlabel('Date')
     plt.ylabel('Price')
 
     plt.title('Interesting shit\nCheck it out')
-    plt.subplots_adjust(left=0.09, bottom=0.18, right=0.94,top=0.90,wspace=0.2,hspace=0)
+    plt.subplots_adjust(left=0.09, bottom=0.18, right=0.94,
+                        top=0.90, wspace=0.2, hspace=0)
     plt.legend()
     plt.show()
 
