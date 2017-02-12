4dfe8824021cb9c0e0baba08d4ea9663f953bd9f
thanator <www.godx@mail.ru>
2017-02-12 21:47:12 +0300

    16 - basic animation

--- test_1.py
+++ test_1.py
@@ -1,76 +1,26 @@
 import matplotlib.pyplot as plt
-import matplotlib.ticker as mticker
-from matplotlib.finance import candlestick_ohlc
-import numpy as np
-import urllib
-import matplotlib.dates as mdates
-import datetime as dt
+import matplotlib.animation as animation
 from matplotlib import style
 
 style.use('seaborn')
 
-print(plt.style.available)
-print(plt.__file__)
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
 
-def bytespdate2num(fmt, encoding='utf-8'):
-    strconventer = mdates.strpdate2num(fmt)
+def animate(i):
+    graph_data = open('sample.txt','r').read()
+    lines = graph_data.split('\n')
+    xs = []
+    ys = []
+    for line in lines:
+        if len(line)>1:
+            x, y = line.split(',')
+            xs.append(x)
+            ys.append(y)
 
-    def bytesconventer(b):
-        s = b.decode(encoding)
-        return strconventer(s)
-    return bytesconventer
+    ax1.clear()
+    ax1.plot(xs,ys)
 
+ani = animation.FuncAnimation(fig, animate, interval = 1000)
 
-def graph_data(stock):
-
-    fig = plt.figure()
-    ax1 = plt.subplot2grid((1, 1), (0, 0))
-
-    stock_price_url = 'http://chartapi.finance.yahoo.com/instrument/1.0/' + \
-        stock + '/chartdata;type=quote;range=1m/csv'
-
-    source_code = urllib.request.urlopen(stock_price_url).read().decode()
-
-    stock_data = []
-    split_source = source_code.split('\n')
-    for line in split_source:
-        split_line = line.split(',')
-        if len(split_line) == 6:
-            if 'values' not in line and 'labels' not in line:
-                stock_data.append(line)
-
-    date, closep, highp, lowp, openp, volume = np.loadtxt(stock_data,
-                                                          delimiter=',',
-                                                          unpack=True,
-                                                          converters={0: bytespdate2num('%Y%m%d')})
-
-    x = 0
-    y = len(date)
-    ohlc = []
-
-    while x < y:
-        append_me = date[x], openp[x], highp[x], lowp[x], closep[x], volume[x]
-        ohlc.append(append_me)
-        x += 1
-
-   # candlestick_ohlc(ax1, ohlc, width=0.4,
-    #                 colorup='#77d879', colordown='#db3f3f')
-    
-    ax1.plot(date,closep)
-    ax1.plot(date,openp)
-    for label in ax1.xaxis.get_ticklabels():
-        label.set_rotation(45)
-
-    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
-    ax1.xaxis.set_major_locator(mticker.MaxNLocator(10))
-
-    plt.xlabel('Date')
-    plt.ylabel('Price')
-    plt.title(stock)
-    plt.subplots_adjust(left=0.09, bottom=0.18, right=0.94,
-                        top=0.90, wspace=0.2, hspace=0)
-    plt.legend()
-    plt.show()
-
-
-graph_data('EBAY')
+plt.show()
