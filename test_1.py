79279ec4a2a2cc3d9dd07537689409b17fa8363d
thanator <www.godx@mail.ru>
2017-02-12 19:04:18 +0300

    14 - candlestick OHLC

--- test_1.py
+++ test_1.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+from matplotlib.finance import candlestick_ohlc
 import numpy as np
 import urllib
 import matplotlib.dates as mdates
@@ -20,7 +22,7 @@ def graph_data(stock):
     ax1 = plt.subplot2grid((1, 1), (0, 0))
 
     stock_price_url = 'http://chartapi.finance.yahoo.com/instrument/1.0/' + \
-        stock + '/chartdata;type=quote;range=10y/csv'
+        stock + '/chartdata;type=quote;range=1m/csv'
 
     source_code = urllib.request.urlopen(stock_price_url).read().decode()
 
@@ -32,42 +34,31 @@ def graph_data(stock):
             if 'values' not in line and 'labels' not in line:
                 stock_data.append(line)
 
-    # date, closep, highp, lowp, openp, volume = np.loadtxt(stock_data,
-    #                                                       delimiter=',',
-    #                                                       unpack=True)
-
-    # dateconv = np.vectorize(dt.datetime.fromtimestamp)
-    # date = dateconv(date)
     date, closep, highp, lowp, openp, volume = np.loadtxt(stock_data,
                                                           delimiter=',',
                                                           unpack=True,
                                                           converters={0: bytespdate2num('%Y%m%d')})
 
-    ax1.plot_date(date, closep, '-', label='Price')
-    ax1.plot([], [], linewidth=5, label='loss', color='r', alpha=0.5)
-    ax1.plot([], [], linewidth=5, label='gain', color='g', alpha=0.5)
-    ax1.axhline(closep[0],color='k',linewidth=2.5)
-    ax1.fill_between(date, closep, closep[0], where=(
-        closep > closep[0]), facecolor='g', alpha=0.3)
-    ax1.fill_between(date, closep, closep[0], where=(
-        closep < closep[0]), facecolor='r', alpha=0.3)
+    x = 0
+    y = len(date)
+    ohlc = []
+
+    while x < y:
+        append_me = date[x], openp[x], highp[x], lowp[x], closep[x], volume[x]
+        ohlc.append(append_me)
+        x += 1
+
+    candlestick_ohlc(ax1, ohlc, width=0.4,
+                     colorup='#77d879', colordown='#db3f3f')
 
     for label in ax1.xaxis.get_ticklabels():
         label.set_rotation(45)
-    ax1.grid(True)  # , color='g',linestyle='-')
-    #ax1.xaxis.label.set_color('c')
-    #ax1.yaxis.label.set_color('r')
-    ax1.set_yticks([0, 10, 20, 30, 40])
+
+    ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    ax1.xaxis.set_major_locator(mticker.MaxNLocator(10))
+
     plt.xlabel('Date')
     plt.ylabel('Price')
-
-    ax1.spines['left'].set_color('c')
-    ax1.spines['right'].set_visible(False)
-    ax1.spines['top'].set_visible(False)
-
-    ax1.spines['left'].set_linewidth(5)
-    ax1.tick_params (axis='x',colors = '#f06215')
-
     plt.title(stock)
     plt.subplots_adjust(left=0.09, bottom=0.18, right=0.94,
                         top=0.90, wspace=0.2, hspace=0)
