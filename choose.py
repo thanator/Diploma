87792b4fd40dff0f6e256e9f25ccb10d279b614a
thanator <www.godx@mail.ru>
2017-02-14 10:27:11 +0300

    I CAN DRAW!

--- choose.py
+++ choose.py
@@ -1,45 +1,74 @@
+import matplotlib.pyplot as plt
+
+
 import xlrd
 import xlwt
-from graphics import *
+
 
 RB = xlrd.open_workbook('book_1.xls', formatting_info=True)
 # выбираем активный лист
 SHEET = RB.sheet_by_index(0)
 
 # COL_OF_YACH = SHEET.row_values(1, 2, 3) -> 2
-class Yach:
 
-    def __init__(self):
-        self.iks = []
-        self.igrik = []
-        self.number = 0
 
 NUM_STROK = len(SHEET.col_values(6, 3))
 
-# YACH_1.iks = SHEET.col_values(4, 3) -> x
-# YACH_1.igrik = SHEET.col_values(5, 3) -> y
-
-#a = [ [[10, 20],1], [[11, 22],2] ]
-# print(a[0][0][1]) -> 20
-# print(a[0][1]) -> 1
-# print(a[0][0][1]) -> 20
 
 i = 0
 
-Kord_setka = [0]*NUM_STROK
+Kord_setka = [0] * NUM_STROK
 for i in range(NUM_STROK):
-    Kord_setka[i] = [0]*2
-    Kord_setka[i][0] = [0]*2
+    Kord_setka[i] = [0] * 2
+    Kord_setka[i][0] = [0] * 2
 
-
-
+num_yach = list(set())
 i = 0
+kol_yach = 0
 while i < NUM_STROK:
 
-    Kord_setka[i][1] = SHEET.row_values(i+3, 6, 7)
-    Kord_setka[i][0][0] = SHEET.row_values(i+3, 4, 5) # ->X
-    Kord_setka[i][0][1] = SHEET.row_values(i+3, 5, 6) # ->Y
+    Kord_setka[i][1] = SHEET.row_values(i + 3, 6, 7)    # ->Num
+    if i != 0 and Kord_setka[i][1] != Kord_setka[i - 1][1]:
+        kol_yach += 1
+    Kord_setka[i][0][0] = SHEET.row_values(i + 3, 4, 5)  # ->X
+    Kord_setka[i][0][1] = SHEET.row_values(i + 3, 5, 6)  # ->Y
     i += 1
 
 
-print(Kord_setka)
+x = [0] * kol_yach
+y = [0] * kol_yach
+
+i = 0
+k = 1
+j = 0
+schet = 0
+for i in range(kol_yach):
+    while k == Kord_setka[j][1][0]:
+        j += 1
+        schet += 1
+    schet += 1
+    x[i] = [0] * schet
+    y[i] = [0] * schet
+    schet = 0
+    k += 1
+
+k = 1
+j = 0
+i = 0
+l = 0
+for i in range(kol_yach):
+    while k == Kord_setka[j][1][0]:
+        x[i][l] = Kord_setka[j][0][0]
+        y[i][l] = Kord_setka[j][0][1]
+        if k != Kord_setka[j+1][1][0]:
+            x[i][l+1] = x[i][0]
+            y[i][l+1] = y[i][0]
+        l += 1
+        j += 1
+    l = 0
+    k += 1
+
+for i in range(kol_yach):
+    plt.plot(x[i], y[i])
+
+plt.show()
