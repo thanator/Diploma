cb35290de2fef4ea0df33823afb79cf9614a97df
thanator <www.godx@mail.ru>
2017-02-25 21:51:19 +0300

    Podschet_ploshadi

--- choose.py
+++ choose.py
@@ -27,7 +27,8 @@ def Setka_to_XY(X_temp, Y_temp, Setka):
             # занесение из входного массива в массив выводной
             x_all += Setka[j_temp][0][0][0]
             y_all += Setka[j_temp][0][1][0]
-            # суммирование всех координат (для дальнейшего рассчёта центральной точки)
+            # суммирование всех координат (для дальнейшего рассчёта центральной
+            # точки)
             l_temp += 1
             if k_temp != Setka[j_temp + 1][1][0]:
                 x_all = x_all / l_temp
@@ -49,6 +50,27 @@ def Setka_to_XY(X_temp, Y_temp, Setka):
         l_temp = 0
         k_temp += 1
 
+# функция для вычисления площади
+
+
+def Plosh(X_temp, Y_temp, S_temp, Koef_temp):
+    i_temp = 0
+    k_temp = 1
+    j_temp = 0
+    schet_temp = 0
+    for i_temp in range(kol_yach):
+        while k_temp == Kord_setka[j_temp][1][0]:
+            if k_temp == Kord_setka[j_temp + 1][1][0]:
+                S_temp[i_temp] += (X_temp[i_temp][schet_temp][0] - X_temp[i_temp][schet_temp + 1][
+                                   0]) * (Y_temp[i_temp][schet_temp][0] + Y_temp[i_temp][schet_temp + 1][0])
+            else:
+                S_temp[i_temp] += (X_temp[i_temp][schet_temp][0] - X_temp[i_temp][0][0]) * (
+                    Y_temp[i_temp][schet_temp][0] + Y_temp[i_temp][0][0])
+                S_temp[i_temp] = S_temp[i_temp] / 2
+            j_temp += 1
+            schet_temp += 1
+        schet_temp = 0
+        k_temp += 1
 
 RB = xlrd.open_workbook('book_1.xls', formatting_info=True)
 # выбираем активный лист
@@ -68,6 +90,8 @@ for i in range(NUM_STROK):
     Kord_setka[i][0] = [0] * 2
     # создание массива для считывания экселевского файла
 
+Koef_t_Anamorph = []
+
 num_yach = list(set())
 i = 0
 kol_yach = 0
@@ -75,15 +99,23 @@ while i < NUM_STROK:
 
     Kord_setka[i][1] = SHEET.row_values(i + 3, 6, 7)    # ->Num
     if i != 0 and Kord_setka[i][1] != Kord_setka[i - 1][1]:
+        Koef_t_Anamorph += SHEET.row_values(i + 2, 7, 8)
         kol_yach += 1
     Kord_setka[i][0][0] = SHEET.row_values(i + 3, 4, 5)  # ->X
     Kord_setka[i][0][1] = SHEET.row_values(i + 3, 5, 6)  # ->Y
     i += 1
 # считывание экселевского файла
+sis = 0
+for pis in range(kol_yach):
+    sis += Koef_t_Anamorph[pis]
 
+sis = (sis / (len(Koef_t_Anamorph)))
+
+Koef_t_Anamorph.append(sis)
+# ПОдсчёт коэф-то анаморфирования
 x = [0] * kol_yach
 y = [0] * kol_yach
-
+# будущие Х и У
 i = 0
 k = 1
 j = 0
@@ -100,7 +132,20 @@ for i in range(kol_yach):
 # создание массива для будущего вывода
 Setka_to_XY(x, y, Kord_setka)
 
+# вызов подсчёта площади
+
+Ploshad = [0] * kol_yach
+
+Plosh(x, y, Ploshad, 1)
+
+#
+
+
 for i in range(kol_yach):
     plt.plot(x[i], y[i])
 
 plt.show()
+
+
+for pis in range(len(Koef_t_Anamorph)):
+    print(Koef_t_Anamorph[pis])
